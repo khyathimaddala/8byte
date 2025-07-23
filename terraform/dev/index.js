de0dd620257adb90de99b39bbe06639b18e158bb
Khyathi Maddala <maddalakhyathi@gmail.com>
2025-07-23 23:47:42 +0530

    change in index.js

--- terraform/dev/index.js
+++ terraform/dev/index.js
@@ -3,4 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, '0.0.0.0', () => console.log('Server running on port 3000'));
+
