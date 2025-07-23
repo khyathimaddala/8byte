f953f84b64347d11398d98ba980ef7e6fdaaebed
Khyathi Maddala <maddalakhyathi@gmail.com>
2025-07-23 19:22:16 +0530

    updated expectation

--- terraform/dev/example.test.js
+++ terraform/dev/example.test.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.static('public'));
 
 describe('Basic Server Test', () => {
-  test('should serve index.html', async () => {
+  test('should serve public/index.html', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toContain('8bytes Application');
+    expect(response.text).toContain('Hey, Hi 8byte - This is Khyathi Maddala'); // Updated expectation
   });
 });
